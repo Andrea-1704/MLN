92ac690fcc7c87a679fc7b97146454df9e80fecb
Andrea-1704 <s327644@studenti.polito.it>
2025-12-01 14:31:05 +0100

    refactoring structure of the repository

---
+++ Executor.py
@@ -0,0 +1,22 @@
+from Rules import MLNRule, rule_friends_logic, rule_smokes_logic
+from Utils import get_all_ground_atoms, un_normalized_world_probability, compute_partition_function
+
+if __name__ == "__main__":
+    my_mln = [
+        MLNRule(2.0, rule_friends_logic, "Friendships_smokes_correlation"),
+        MLNRule(0.5, rule_smokes_logic, "Smokes")
+    ]
+    example_world = {
+        "Friends(Anna,Bob)": True,
+        "Smokes(Anna)": True,
+        "Smokes(Bob)": False,
+        "Friends(Bob,Charlie)": True,
+        "Smokes(Charlie)": True
+    }
+
+    weight = un_normalized_world_probability(example_world, my_mln)
+    partition_function = compute_partition_function(my_mln, get_all_ground_atoms(['Anna', 'Bob', 'Charlie'], ['Smokes/1', 'Friends/2']))
+    prob = weight / partition_function
+
+    print(f"Probability of the example world: {prob}")
+    print(f"Partition function Z: {partition_function}")

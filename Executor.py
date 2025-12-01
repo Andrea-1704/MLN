4c2b782d3f05418d1a0c44e9b58937755e8907ff
Andrea-1704 <s327644@studenti.polito.it>
2025-12-01 15:28:16 +0100

    Approved version

--- Executor.py
+++ Executor.py
@@ -6,31 +6,16 @@ if __name__ == "__main__":
         MLNRule(2.0, rule_friends_logic, "Friendships_smokes_correlation"),
         MLNRule(0.5, rule_smokes_logic, "Smokes")
     ]
-    example_world = {
-        "Friends(Anna,Bob)": True,
-        "Smokes(Anna)": True,
-        "Smokes(Bob)": False,
-        "Friends(Bob,Charlie)": True,
-        "Smokes(Charlie)": True
-    }
-
-    weight = un_normalized_world_probability(example_world, my_mln)
-    partition_function = compute_partition_function(my_mln, get_all_ground_atoms(['Anna', 'Bob', 'Charlie'], ['Smokes/1', 'Friends/2']))
-    prob = weight / partition_function
-
-    print(f"Probability of the example world: {prob}")
-    print(f"Partition function Z: {partition_function}")
-    target_query = "Smokes(Anna)"
     my_constants = ['Anna', 'Bob', 'Charlie']
     my_predicates = ['Smokes/1', 'Friends/2']
+
+    target_query = "Smokes(Anna)"
     prob = compute_marginal(target_query, my_constants, my_predicates, my_mln)
     print(f"The probability that {target_query} is True is: {prob:.4f}")
 
-    target_query = "Friends(Anna,Anna)"
-    prob = compute_marginal(target_query, my_constants, my_predicates, my_mln)
-    print(f"The probability that {target_query} is True is: {prob:.4f}")
 
     query = "Smokes(Anna)"
     conditioning_query = ["Friends(Anna,Bob)", "Smokes(Bob)"]
+    conditioning_query = ["Smokes(Anna)"]
     cond_prob = compute_conditional_probability(query, conditioning_query, my_constants, my_predicates, my_mln)
     print(f"P({query} | {conditioning_query}) = {cond_prob:.4f}")
